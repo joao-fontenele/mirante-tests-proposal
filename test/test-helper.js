3309eb15ffe07bcb1ae695f55e16720abaafda8b
jpaulofb <jpfb@icomp.ufam.edu.br>
2016-11-07 16:03:34 -0400

    adding jsdom lib and jsdom setup
    jsdom is needed to use the mount function of enzyme, and simulates a full mount
    in a browser environment

--- test/test-helper.js
+++ test/test-helper.js
@@ -4,6 +4,7 @@ var React = require('react');
 var chaiEnzyme = require('chai-enzyme');
 var sinon = require('sinon');
 var sinonChai = require('sinon-chai');
+var jsdom = require('jsdom').jsdom;
 
 chai.use(chaiEnzyme());
 chai.use(sinonChai);
@@ -11,6 +12,18 @@ chai.use(sinonChai);
 var expect = chai.expect;
 var shallow = enzyme.shallow;
 var mount = enzyme.mount;
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach(function(property) {
+  if (typeof global[property] === 'undefined') {
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
 
 global.React = React;
 global.expect = expect;
